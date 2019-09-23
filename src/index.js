4f08fd71bc45ec682fad9b9dcb49c37076dc9354
Joona Laine <joona.laine@spatineo.com>
2019-09-23 15:25:01 +0300

    Hide favicon temporarily and use App directly without Gateway

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Gateway from './Gateway';
+import {App} from "./App";
+
 
 document.addEventListener('DOMContentLoaded', () => {
     ReactDOM.render(
-        React.createElement(Gateway),
+        React.createElement(App),
         document.getElementById('app')
     );
 });
