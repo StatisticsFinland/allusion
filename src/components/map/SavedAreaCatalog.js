ba9e506e0c8de7ae4c0d80cf3178f8b897e01cb9
Joona Laine <joona.laine@spatineo.com>
2019-10-09 16:51:17 +0300

    Modify translations a bit

--- src/components/map/SavedAreaCatalog.js
+++ src/components/map/SavedAreaCatalog.js
@@ -10,11 +10,13 @@ import ListItemText from "@material-ui/core/ListItemText";
 import Share from '@material-ui/icons/Share';
 import Tooltip from "@material-ui/core/Tooltip";
 import queryString from 'query-string';
+import FormControl from "@material-ui/core/FormControl";
+import FormLabel from "@material-ui/core/FormLabel";
 
 
 const styles = theme => ({
   inputPadding: {
-    paddingBottom: '15px'
+    paddingBottom: '20px'
   },
   text: {
     marginTop: 16,
@@ -123,111 +125,120 @@ class SavedAreaCatalog extends Component {
     const {classes, savedAreas, savedCustomAreas, areaName, customAreaName, txt} = this.props;
 
     return (
-        <div>
-          <div style={{display: 'flex', flexDirection: 'column'}}>
+        <>
+          <FormControl classes={{root: classes.inputPadding}}>
+            <div style={{display: 'flex', flexDirection: 'column'}}>
 
-            {savedCustomAreas.length > 0 &&
-            <div key={`div_all_custom_areas`} style={{display: 'flex'}}>
-              <Checkbox key={`checkbox_all_custom_areas`}
-                        color='primary'
-                        style={{paddingTop: 4, paddingBottom: 4}}
-                        checked={savedCustomAreas.every(area => area.activated)}
-                        disabled={savedCustomAreas.some(area => area.beingModified)}
-                        onChange={() => this.props.toggleAllCustomAreas(savedCustomAreas.some(area => !area.activated))}>
-              </Checkbox>
-              <ListItem
-                  className={classes.first}
-                  key={`listitem_all_custom_areas`}
-                  dense
-                  button
-                  disableGutters={true}
-              >
-                <ListItemText key={`listitemtext_all_custom_areas`} primary={txt.igalod.showAll}/>
-              </ListItem>
-            </div>
-            }
-            {savedCustomAreas.sort((a, b) => a.name.localeCompare(b.name)).map(area => {
-              return <div key={`div_${area.id}`} style={{display: 'flex'}}>
-                <Checkbox key={`checkbox_${area.id}`}
+              <FormLabel>{txt.savedAreasDrawer.customUnions}</FormLabel>
+              {savedCustomAreas.length > 0 &&
+              <div key={`div_all_custom_areas`} style={{display: 'flex'}}>
+                <Checkbox key={`checkbox_all_custom_areas`}
                           color='primary'
                           style={{paddingTop: 4, paddingBottom: 4}}
-                          checked={area.activated}
+                          checked={savedCustomAreas.every(area => area.activated)}
                           disabled={savedCustomAreas.some(area => area.beingModified)}
-                          onChange={() => this.props.toggleCustomAreaSelection(area)}>
-
+                          onChange={() => this.props.toggleAllCustomAreas(savedCustomAreas.some(area => !area.activated))}>
                 </Checkbox>
                 <ListItem
                     className={classes.first}
-                    key={`listitem_${area.id}`}
+                    key={`listitem_all_custom_areas`}
                     dense
                     button
                     disableGutters={true}
-                    aria-haspopup="true"
-                    aria-controls="placesearch"
-                    aria-label="Valitse kunta"
-                    onClick={() => null}
-                    onDoubleClick={() => this.startModifyingCustomArea(area)}>
-                  {this.populateListItem(area)}
+                >
+                  <ListItemText key={`listitemtext_all_custom_areas`} primary={txt.igalod.showAll}/>
                 </ListItem>
               </div>
-            })}
+              }
+              {savedCustomAreas.sort((a, b) => a.name.localeCompare(b.name)).map(area => {
+                return <div key={`div_${area.id}`} style={{display: 'flex'}}>
+                  <Checkbox key={`checkbox_${area.id}`}
+                            color='primary'
+                            style={{paddingTop: 4, paddingBottom: 4}}
+                            checked={area.activated}
+                            disabled={savedCustomAreas.some(area => area.beingModified)}
+                            onChange={() => this.props.toggleCustomAreaSelection(area)}>
 
-            <TextField
-                inputProps={{name: 'customAreaName', id: 'customAreaName'}}
-                label={txt.igalod.newCustomArea}
-                defaultValue={customAreaName}
-                value={customAreaName}
-                onChange={event => this.props.changeCustomAreaName(event.target.value)}
-                onKeyPress={(ev) => {
-                  if (ev.key === 'Enter') {
-                    this.saveNewCustomArea(customAreaName);
-                    ev.preventDefault();
-                  }
-                }}
-                classes={{root: classes.text}}>
-            </TextField>
+                  </Checkbox>
+                  <ListItem
+                      className={classes.first}
+                      key={`listitem_${area.id}`}
+                      dense
+                      button
+                      disableGutters={true}
+                      aria-haspopup="true"
+                      aria-controls="placesearch"
+                      aria-label="Valitse kunta"
+                      onClick={() => null}
+                      onDoubleClick={() => this.startModifyingCustomArea(area)}>
+                    {this.populateListItem(area)}
+                  </ListItem>
+                </div>
+              })}
+
+              <TextField
+                  inputProps={{name: 'customAreaName', id: 'customAreaName'}}
+                  label={txt.savedAreasDrawer.newUnion}
+                  defaultValue={customAreaName}
+                  value={customAreaName}
+                  onChange={event => this.props.changeCustomAreaName(event.target.value)}
+                  onKeyPress={(ev) => {
+                    if (ev.key === 'Enter') {
+                      this.saveNewCustomArea(customAreaName);
+                      ev.preventDefault();
+                    }
+                  }}
+                  classes={{root: classes.text}}>
+              </TextField>
 
 
-          </div>
-          <div key={`div_custom_area_buttons`} style={{display: 'flex'}}>
+            </div>
+            <div key={`div_custom_area_buttons`} style={{display: 'flex'}}>
+              <Button color='primary'
+                      onClick={() => this.saveNewCustomArea(customAreaName)}>{txt.button.saveNew}</Button>
+
+              <Divider orientation="vertical"/>
+              <Tooltip id="infoButtonTooltip" title={txt.shareDialog.tooltip} placement={"bottom-start"}>
+                <Button size='small' onClick={() => this.shareCustomAreas()}>
+                  <Share/>
+                </Button>
+              </Tooltip>
+            </div>
+            {/*<div key={`div_custom_area_buttons_bring`} style={{display: 'flex'}}>
+              <Button size='small'  color='primary'>{txt.savedAreasDrawer.bringMajorRegions}</Button>
+              <Button size='small'  color='primary'>{txt.savedAreasDrawer.bringRegions}</Button>
+            </div>*/}
+            <Divider/>
+          </FormControl>
+
+          <FormControl classes={{root: classes.inputPadding}}>
+            <FormLabel>{txt.savedAreasDrawer.ownAreaSelections}</FormLabel>
+            <div style={{display: 'flex', flexDirection: 'column'}}>
+              {savedAreas.map(area => {
+                return <Chip
+                    clickable
+                    key={area.id}
+                    label={area.name}
+                    onDelete={() => this.props.handleDelete(area.id)}
+                    onClick={() => this.props.activateSelection(area.selection)}
+                    className={classes.chip}
+                    color="secondary"
+                    variant="outlined"
+                />
+              })}
+
+              <TextField
+                  inputProps={{name: 'areaName', id: 'areaName'}}
+                  label={txt.savedAreasDrawer.newArea}
+                  defaultValue={areaName}
+                  onChange={this.props.changeName}
+                  classes={{root: classes.text}}>
+              </TextField>
+            </div>
             <Button color='primary'
-                    onClick={() => this.saveNewCustomArea(customAreaName)}>{txt.button.saveNew}</Button>
-
-            <Divider orientation="vertical"/>
-            <Tooltip id="infoButtonTooltip" title={txt.shareDialog.tooltip} placement={"bottom-start"}>
-              <Button size='small' onClick={() => this.shareCustomAreas()}>
-                <Share/>
-              </Button>
-            </Tooltip>
-          </div>
-
-
-          <Divider/>
-          <div style={{display: 'flex', flexDirection: 'column'}}>
-            {savedAreas.map(area => {
-              return <Chip
-                  clickable
-                  key={area.id}
-                  label={area.name}
-                  onDelete={() => this.props.handleDelete(area.id)}
-                  onClick={() => this.props.activateSelection(area.selection)}
-                  className={classes.chip}
-                  color="secondary"
-                  variant="outlined"
-              />
-            })}
-
-            <TextField
-                inputProps={{name: 'areaName', id: 'areaName'}}
-                label={txt.igalod.newArea}
-                defaultValue={areaName}
-                onChange={this.props.changeName}
-                classes={{root: classes.text}}>
-            </TextField>
-          </div>
-          <Button color='primary'
-                  onClick={() => this.props.saveArea(areaName)}>{txt.button.saveNew}</Button>
-        </div>
+                    onClick={() => this.props.saveArea(areaName)}>{txt.button.saveNew}</Button>
+          </FormControl>
+        </>
     );
   };
 }
