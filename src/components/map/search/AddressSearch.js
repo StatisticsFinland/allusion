d689feb95e1eb4c48d8579d343bedcecd8b8873f
Joona Laine <joona.laine@spatineo.com>
2019-10-30 13:41:20 +0200

    Replace mapbox geocoding API with digitransit autocomplete API

--- src/components/map/search/AddressSearch.js
+++ src/components/map/search/AddressSearch.js
@@ -1,5 +1,5 @@
-import React, { Component } from 'react';
-import { withStyles } from '@material-ui/core/styles';
+import React, {Component} from 'react';
+import {withStyles} from '@material-ui/core/styles';
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemText from '@material-ui/core/ListItemText';
@@ -16,10 +16,7 @@ const styles = theme => ({
   },
 });
 
-const API = 'https://api.mapbox.com/geocoding/v5/mapbox.places/';
-const access_token = 'access token';
-const country = 'fi';
-const limit =  '10';
+const API = 'http://api.digitransit.fi/geocoding/v1/autocomplete';
 
 class AddressSearch extends Component {
 
@@ -31,72 +28,72 @@ class AddressSearch extends Component {
   button = undefined;
 
   handleChange = event => {
-    this.setState({ inputFieldValue: event.target.value });
-  }
+    this.setState({inputFieldValue: event.target.value});
+  };
 
   handleKeyPress = event => {
     if (event.key === 'Enter') {
       event.preventDefault();
       this.handleSubmit(event);
     }
-  }
+  };
 
   handleSubmit = event => {
-    if(this.state.inputFieldValue && this.state.inputFieldValue !== ''){ 
-      fetch(API + this.state.inputFieldValue + '.json?access_token=' + access_token + '&country=' + country + '&limit=' + limit)
-      .then(response => response.json())
-      .then(data => this.setState({ hits: data.features }));
+    if (this.state.inputFieldValue && this.state.inputFieldValue !== '') {
+      fetch(`${API}?text=${this.state.inputFieldValue}`)
+          .then(response => response.json())
+          .then(data => this.setState({hits: data.features}));
       event.preventDefault();
     }
-  }
+  };
 
   render() {
 
-    const { classes } = this.props;
+    const {classes} = this.props;
 
     return (
 
-      <div className={classes.root}>
+        <div className={classes.root}>
 
-        <form autoComplete="off">
-          <FormControl>
-            <InputLabel htmlFor="address">{this.props.txt.typein}</InputLabel>
-            <Input
-              id="address"
-              type='text'
-              value={this.state.inputFieldValue}
-              onChange={this.handleChange}
-              onKeyPress={this.handleKeyPress}
-              endAdornment={
-                <InputAdornment position="end">
-                  <IconButton
+          <form autoComplete="off">
+            <FormControl>
+              <InputLabel htmlFor="address">{this.props.txt.typein}</InputLabel>
+              <Input
+                  id="address"
+                  type='text'
+                  value={this.state.inputFieldValue}
+                  onChange={this.handleChange}
+                  onKeyPress={this.handleKeyPress}
+                  endAdornment={
+                    <InputAdornment position="end">
+                      <IconButton
+                          aria-label="Search for address"
+                          onClick={this.handleSubmit}
+                      >
+                        <Search/>
+                      </IconButton>
+                    </InputAdornment>
+                  }
+              />
+            </FormControl>
+          </form>
+
+          <List component="ul" dense={false} disablePadding={true}>
+            {this.state.hits.map((element, index) =>
+                <ListItem
+                    key={index}
+                    disableGutters={true}
+                    button
+                    aria-haspopup="true"
+                    aria-controls="addresssearch"
                     aria-label="Search for address"
-                    onClick={this.handleSubmit}
-                  >
-                    <Search />
-                  </IconButton>
-                </InputAdornment>
-              }
-            />
-          </FormControl>
-        </form>
-
-        <List component="ul" dense={false} disablePadding={true}>
-          {this.state.hits.map((element, index) =>
-            <ListItem 
-              key={index}
-              disableGutters={true}
-              button
-              aria-haspopup="true"
-              aria-controls="addresssearch"
-              aria-label="Search for address"             
-              onClick={e => this.props.handleClick(element.center, 'address')}
-            >
-            <ListItemText primary={element.place_name} />
-            </ListItem>
-          )}
-        </List>
-      </div>
+                    onClick={e => this.props.handleClick(element.geometry.coordinates, 'address')}
+                >
+                  <ListItemText primary={element.properties.name}/>
+                </ListItem>
+            )}
+          </List>
+        </div>
 
     );
   }
